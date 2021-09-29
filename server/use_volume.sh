e4ccd00191cdeb13bb765a60a21e11cc127dd60b
Kirill Byvshev <k@byv.sh>
2021-09-29 22:14:48 +0300

    move dirs to volume

--- server/use_volume.sh
+++ server/use_volume.sh
@@ -19,3 +19,17 @@ mkdir -p $VOLUME_DIR/var/lib
 mv /var/lib/docker $VOLUME_DIR/var/lib
 ln -s $VOLUME_DIR/var/lib/docker /var/lib/docker
 systemctl start docker
+
+# Nginx to volume
+mkdir -p $VOLUME_DIR/etc/nginx/sites-available
+mkdir -p $VOLUME_DIR/etc/nginx/sites-enabled
+mv /etc/nginx/sites-available $VOLUME_DIR/etc/nginx/
+mv /etc/nginx/sites-enabled $VOLUME_DIR/etc/nginx/
+ln -s $VOLUME_DIR/etc/nginx/sites-available /etc/nginx/sites-available
+ln -s $VOLUME_DIR/etc/nginx/sites-enabled /etc/nginx/sites-enabled
+
+# Letsencrypt to volume
+mkdir -p $VOLUME_DIR/etc/
+mv /etc/letsencrypt $VOLUME_DIR/etc/letsencrypt
+ln -s $VOLUME_DIR/etc/letsencrypt /etc/letsencrypt
+
